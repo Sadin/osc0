1f987346f3b0f548cec6d325d5f5d15225486cb9
Zachary Snyder <sadin@fedoraproject.org>
2016-04-01 08:04:41 +0000

    Complies with Style guide PEP 8 ( I think )

--- main.py
+++ main.py
@@ -1,32 +1,32 @@
-import gi, random
+import gi
+import random
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk
 
 alphabet = "abcdefghijklmnopqrstuvwxyz"
 
-def genRandom(chars, length):
-	rndAlphabet = ""
-	for x in range(0, length):
-		rndAlphabet = rndAlphabet + random.choice(alphabet)	
-		
-	return rndAlphabet
+def gen_random(chars, length):
+    rnd_alphabet = ""
+    for x in range(0, length):
+        rnd_alphabet += random.choice(alphabet)	
+
+    return rnd_alphabet
 
 class MyWindow(Gtk.Window):
 
 
-	def __init__(self):
-		Gtk.Window.__init__(self, title="Hello World")
+    def __init__(self):
+        Gtk.Window.__init__(self, title="Hello World")
 
-		self.button = Gtk.Button(label="Click Here")
-		self.button.connect("clicked", self.on_button_clicked)
-		self.add(self.button)
+        self.button = Gtk.Button(label="Click Here")
+        self.button.connect("clicked", self.on_button_clicked)
+        self.add(self.button)
 
-	def on_button_clicked(self, widget):
+    def on_button_clicked(self, widget):
 
-		rndAlphabet = genRandom('abcdefghijklmnopqrstuvwxyz', 8)
-#		print(len(alphabet)) # just to check
-		print(rndAlphabet)
-#		print("Hello World")
+        rnd_alphabet = gen_random('abcdefghijklmnopqrstuvwxyz', 8)
+        print(rnd_alphabet)
+
 
 win = MyWindow()
 win.connect("delete-event", Gtk.main_quit)
